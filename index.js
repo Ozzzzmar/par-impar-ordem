7a2e180b9677ce3ad4c5ee81066cc05c772f9908
Osmar <juninhomachado579@gmail.com>
2024-06-17 11:11:34 -0500

    feat: adicionado oragnizar o array

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const inteiros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let inteiros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let pares = [];
+let impares = [];
+
+for(let i = 0; i < inteiros.length; i++){
+    if(inteiros[i] % 2 == 0){
+        pares.push(inteiros[i]);
+    }else{
+        impares.push(inteiros[i]);
+    }
+}
+console.log(...pares, ...impares);
